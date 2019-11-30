82082ea364324d9d4a935a4751a69f1caa6a21e5
nonobstant <56279981+nonobstant@users.noreply.github.com>
2019-11-30 21:04:55 +0100

    bg

--- nebuleuse/js/modal.js
+++ nebuleuse/js/modal.js
@@ -1,12 +1,12 @@
 // OPEN
 var modal = document.getElementById('01');
 var modal2 = document.getElementById('02');
+var body = document.getElementById('body');
 
 // CLICK CLOSE
 window.onclick = function(event) {
-if (event.target == modal) {
+if (event.target == body) {
 modal.style.display = "none";
-} else if (event.target == modal2) {
 modal2.style.display = "none";
-}
+} 
 }
