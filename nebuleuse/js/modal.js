d2f31b89c78daeba6e5c42d9b5c4cfd00c4c37ec
nonobstant <56279981+nonobstant@users.noreply.github.com>
2019-12-02 09:26:28 +0100

    menu + design

--- nebuleuse/js/modal.js
+++ nebuleuse/js/modal.js
@@ -1,12 +1,14 @@
 // OPEN
 var modal = document.getElementById('01');
 var modal2 = document.getElementById('02');
-var body = document.getElementById('body');
+var modal3 = document.getElementById('03');
+var body = document.getElementById('shell');
 
 // CLICK CLOSE
 window.onclick = function(event) {
-if (event.target == body) {
+if (event.target == shell) {
 modal.style.display = "none";
 modal2.style.display = "none";
-} 
+modal3.style.display = "none";
+}
 }
