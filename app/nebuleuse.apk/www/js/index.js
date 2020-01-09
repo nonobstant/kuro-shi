6a80559089ee16d39960469700437947fe283f52
nonobstant <56279981+nonobstant@users.noreply.github.com>
2020-01-09 23:19:06 +0100

    del

--- app/nebuleuse.apk/www/js/index.js
+++
@@ -1,6 +0,0 @@
-jQuery('html,body').bind('mousewheel', function(event) {
-    event.preventDefault();
-    var scrollTop = this.scrollTop;
-    this.scrollTop = (scrollTop + ((event.deltaY * event.deltaFactor) * -1));
-    //console.log(event.deltaY, event.deltaFactor, event.originalEvent.deltaMode, event.originalEvent.wheelDelta);
-  });
--- app/nebuleuse.apk/www/js/menu.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('.menu-icon').click(function () {
-        $(this).toggleClass('is-open');
-    });
-
-    $('.menu-icon-trigger').click(function () {
-        $(this).parent('.menu-icon-svg').toggleClass('is-open');
-    })
-});
